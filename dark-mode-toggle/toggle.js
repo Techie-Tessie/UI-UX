58f9e480f57d55d0ee92fe9f5968bab68919f8ad
Tess Watt <71218139+Tess314@users.noreply.github.com>
2021-06-12 20:33:41 +0100

    Update toggle.js

--- dark-mode-toggle/toggle.js
+++ dark-mode-toggle/toggle.js
@@ -1,1 +1,13 @@
-
+//changes colour scheme to dark mode when toggle switch checked
+function darkMode()
+{
+				var checkBox = document.getElementById("toggle");
+				if (checkBox.checked == true)
+				{
+					document.body.style.backgroundColor = "black";
+				}
+				else
+				{
+					document.body.style.backgroundColor = "white";
+  				}
+}
