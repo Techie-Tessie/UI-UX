97d8660dbef11c4d677ef149ffc1590124f96b9c
Tess <71218139+Tess314@users.noreply.github.com>
2021-06-16 18:38:27 +0100

    Create text.js

---
+++ text-formatting/text.js
@@ -0,0 +1,11 @@
+//changes text colour on button click
+function changeColour()
+{
+	document.getElementById("text").style.color="red";
+}
+
+//changes text size on button click
+function changeSize()
+{
+	document.getElementById("text").style.fontSize = "100px";
+}
